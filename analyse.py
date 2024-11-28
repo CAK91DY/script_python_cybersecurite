63f78e926bd25001d2a3a4b41aa265d4e4a4db8a
cakpo christophe <cakpochristophes91@gmail.com>
2024-11-28 15:52:35 +0100

    utilisation des regex et pandas

--- analyse.py
+++ analyse.py
@@ -1,23 +1,40 @@
 import argparse
-from module.log_reader import LogsReader
+from modules.log_reader import LogReader
+#from modules.log_analyzer import LogAnalyzer
+
 def main():
     # Gestion des arguments en ligne de commande
     parser = argparse.ArgumentParser(description="Script d'analyse de logs")
     parser.add_argument("repertoire", help="Chemin vers le répertoire contenant les fichiers de logs", type=str)
+    parser.add_argument("--pattern", help="Pattern pour filtrer les fichiers de logs (par défaut 'secure*')", type=str, default="secure*")
+    parser.add_argument("--seuil", help="Seuil d'alerte pour les adresses IP suspectes", type=int, default=100)
     args = parser.parse_args()
-    
-    # créer une instance de LogReader avec le chemin du repertoire
-    lecture = LogsReader(args.repertoire)
-    fichiers_logs = lecture.trouver_fichiers_logs()
 
+    # Créer une instance de LogReader avec le chemin du répertoire
+    lecteur = LogReader(args.repertoire)
+
+    # Trouver tous les fichiers de logs correspondant au pattern dans le répertoire
+    fichiers_logs = lecteur.trouver_fichiers_logs(pattern=args.pattern)
+
+    # Si des fichiers de logs sont trouvés, les lire un par un et extraire les informations
     if fichiers_logs:
-        for fichiers_log in fichiers_logs:
-            print(f"\nLecture du fichier : {fichiers_log}")
-            lecture.lire_logs(fichiers_log)
+        for fichier_log in fichiers_logs:
+            print(f"\nLecture du fichier : {fichier_log}")
+            lecteur.lire_et_extraire_logs(fichier_log)  # Lire et extraire les informations du fichier de logs
 
-        lecture.affichier_lignes_lues()
+        # Créer le DataFrame une fois que tous les fichiers ont été lus
+        lecteur.creer_dataframe()
 
+        # Créer une instance de LogAnalyzer pour analyser les logs
+        #analyseur = LogAnalyzer(lecteur.df_logs)
+
+        # Analyser la fréquence des adresses IP
+        #analyseur.analyser_frequence_ips(seuil_alerte=args.seuil)
+
+        # Afficher le DataFrame contenant les informations extraites
+        lecteur.afficher_dataframe()
     else:
-        print("Aucun fichier de logs trouver dans le repertoire")
+        print(f"Aucun fichier de logs correspondant au pattern '{args.pattern}' n'a été trouvé dans le répertoire.")
+
 if __name__ == "__main__":
     main()
